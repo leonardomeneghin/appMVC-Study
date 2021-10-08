428c37bab9fe760099c38196a0ee39e6eefcc42f
leonardo <leonardozenni@gmail.com>
2021-10-08 10:47:03 -0300

    Projeto inicial

---
+++ appMVC/Models/Context.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace appMVC.Models
+{
+    public class Context : DbContext
+    {
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            //Método que configura o EntityFramework
+            //Aqui dizemos qual DB será usado
+
+            optionsBuilder.UseSqlServer(@"Data Source=SALPC0062\DB01LOCAL;Initial Catalog=BItest;Integrated Security=True");
+            
+
+        }
+
+    }
+}
---
+++ appMVC/Startup.cs
@@ -0,0 +1,61 @@
+using appMVC.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace appMVC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            //Deixar o servidor visivel
+            services.AddDbContext<Context>();
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())  //Valida se está em ambiente de desenvolvimento
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts(); //Ativa se não for ambiente de desenvolvimento
+            }
+            app.UseHttpsRedirection(); //Redireciona pra HTTPS em caso de tentativa de HTTP
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"); //Rota ao site
+            });
+        }
+    }
+}
